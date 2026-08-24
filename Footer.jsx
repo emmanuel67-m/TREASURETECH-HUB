2264871c28b4b69877c0942fe3492cd5b455deb9
CodeCrafter <emmanuelabioye67@gmail.com>
2026-08-24 04:41:39 -0700

    Add files via upload

--- Footer.jsx
+++ Footer.jsx
@@ -28,16 +28,25 @@ function Footer() {
           <div className="flex items-center space-x-2 text-sm text-gray-300">
             <FaEnvelope />
             <a
-              href="mailto:susandorothy552@gmail.com"
+              href="mailto:sandraevelyn524@gmail.com"
               className="hover:text-white transition-colors duration-300"
             >
-              susandorothy552@gmail.com
+              sandraevelyn524@gmail.com
+            </a>
+          </div>
+          <div className="flex items-center space-x-2 text-sm text-gray-300">
+            <FaEnvelope />
+            <a
+              href="mailto:sandraevelyn524@gmail.com"
+              className="hover:text-white transition-colors duration-300"
+            >
+              Susandorothy@gmail.com
             </a>
           </div>
           <div className="flex items-center space-x-2 text-sm text-gray-300 mt-2">
             <FaPhone />
             <a href="tel:+1234567890" className="hover:text-white transition-colors duration-300">
-              +1 (661) 470-4312
+              +1 (269) 251-1408
             </a>
           </div>
         </div>
@@ -143,3 +152,4 @@ function Footer() {
 }
 
 export default Footer;
+
